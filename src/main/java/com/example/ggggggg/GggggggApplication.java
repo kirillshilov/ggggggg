daf3b883cbc3de975b13a8ff3d3077970676c686
kirillshilov <sarataza@inbox.ru>
2022-05-04 20:01:32 +0300

    initdffffffffffffffffffffffff

---
+++ src/main/java/com/example/ggggggg/GggggggApplication.java
@@ -0,0 +1,13 @@
+package com.example.ggggggg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GggggggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GggggggApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ggggggg/GggggggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ggggggg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GggggggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
