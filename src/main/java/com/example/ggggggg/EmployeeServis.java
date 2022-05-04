aeb99f28a68df79a5da14e76eb198b7f4a480857
kirillshilov <sarataza@inbox.ru>
2022-05-04 20:04:20 +0300

    initdffffffffffffffffffffffff

---
+++ src/main/java/com/example/ggggggg/EmployeeServis.java
@@ -0,0 +1,11 @@
+package com.example.ggggggg;
+
+public interface EmployeeServis {
+
+    public Employee addEmployee(String firstName, String lastName);
+
+    public Employee deleteEmployee(String firstName, String lastName);
+
+    public Employee findEmployee(String firstName, String lastName);
+
+}
