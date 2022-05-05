033ac3e286957b8bb3616d18e9d1c6a8748c178b
kirillshilov <sarataza@inbox.ru>
2022-05-05 10:08:03 +0300

    Переделал все как ты сказал. Надеюсь теперь все нормально

--- src/main/java/com/example/ggggggg/OutOfBounds.java
+++
@@ -1,8 +0,0 @@
-package com.example.ggggggg;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
-public class OutOfBounds extends ArrayIndexOutOfBoundsException  {
-}
